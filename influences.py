31dbbe24317dc121557d4c0c4de81313d42e6d70
sebastianvarelab <svarela@miuandes.cl>
2021-04-17 21:10:16 -0400

    inicio_influencias

--- influences.py
+++ influences.py
@@ -1,9 +1,9 @@
 from main import Board
-if n_players==4:
-    class influences (self,player_1,player_2,player_3,player_4):
+class influences (self,player_1,player_2,player_3,player_4):
+    if n_players==4:
         if Board.turn==1:
             print(player_1.name)
-            p_1=int(input ("wich card do you whant to play; 1=Duke, 2=Murderer, 3=Captain, 4=Ambassador, 5=Coup, 6=Income, 7=Foreing aid")
+            p_1=int(input ("wich card do you whant to play; 1=Duke, 2=Murderer, 3=Captain, 4=Ambassador, 5=Coup, 6=Income, 7=Foreing aid"))
             if p_1==1:
                 print (player_1.name, "choose to play Duke")  
             elif p_1==2:
@@ -22,6 +22,7 @@ if n_players==4:
                 print (player_1.name)
                 attack=input ("wich player do you whant to attack?")
                 print (attack)
+            #bajar esos
             elif p_1==4:
                 #permitir ver dos cartas del mazo y elegir con cuales se queda
             elif p_1==1:
@@ -36,10 +37,9 @@ if n_players==4:
                 print(player_4.name)
                 p_4=int(input("Do you whant to do a challenge; 1 yes 2 no")
                 #hacer random con los que decidieron el challenge y si no hay listo
-
             elif p_1==5:
                 player_1.coins-=7
-                k.o=input("wich player lose 1 influence")
+                k_o=input("wich player lose 1 influence")
                 #hacer elegir al jugador atacado que carta pierde 
             elif p_1==6:
                 player_1.coins +=1
