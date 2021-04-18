43da84fd9b72b5353d2a3aeebd33e2c22413ce74
sebastianvarelab <svarela@miuandes.cl>
2021-04-17 22:18:40 -0400

    avance_influencias

--- influences.py
+++ influences.py
@@ -1,5 +1,6 @@
 from main import Board
-class influences (self,player_1,player_2,player_3,player_4):
+import random
+class influences:
     if n_players==4:
         if Board.turn==1:
             print(player_1.name)
@@ -22,21 +23,51 @@ class influences (self,player_1,player_2,player_3,player_4):
                 print (player_1.name)
                 attack=input ("wich player do you whant to attack?")
                 print (attack)
-            #bajar esos
-            elif p_1==4:
-                #permitir ver dos cartas del mazo y elegir con cuales se queda
-            elif p_1==1:
-                player_1.coins +=3 
             else:
                 continue
             if p_1==1 or p_1==2 or p_1==3 or p_1==4:
                 print(player_2.name)
-                p_2=int(input("Do you whant to do a challenge; 1 yes 2 no")
+                p_2=int(input("Do you whant to do a challenge; 1 yes 2 no"))
                 print(player_3.name)
-                p_3=int(input("Do you whant to do a challenge; 1 yes 2 no")
+                p_3=int(input("Do you whant to do a challenge; 1 yes 2 no"))
                 print(player_4.name)
-                p_4=int(input("Do you whant to do a challenge; 1 yes 2 no")
-                #hacer random con los que decidieron el challenge y si no hay listo
+                p_4=int(input("Do you whant to do a challenge; 1 yes 2 no"))
+                #hacer que el jugador demuestre tener la carta en el challenge
+                list_challenge=[]
+                if p_2==1 and p_3==1 and p_4==1:
+                    list_challenge.append(player_2.name)
+                    list_challenge.append(player_3.name)
+                    list_challenge.append(player_4.name)
+                    random.shuffle(list_challenge)
+                    print(list_challenge[0], "you do the challenge, good luck")
+                    if p_1==1:
+                        if Board.cards_player_1[0]=="DUKE" or Board.cards_player_1[1]=="DUKE":
+                            print(list_challenge[0], "you lose the challenge")
+                            if players_1.cards[0]=="DUKE":
+                                ...
+
+                elif p_2==1 and p_3==1:
+                    list_challenge.append(player_2.name)
+                    list_challenge.append(player_3.name)
+                    random.shuffle(list_challenge)
+                    print(list_challenge[0], "you do the challenge, good luck")
+                elif p_3==1 and p_4==1:
+                    list_challenge.append(player_3.name)
+                    list_challenge.append(player_4.name)
+                    random.shuffle(list_challenge)
+                    print(list_challenge[0], "you do the challenge, good luck")
+                elif p_2==1:
+                    list_challenge.append(player_2.name)
+                    print(player_2.name, "you do the challenge, good luck")
+                elif p_3==1:
+                    list_challenge.append(player_3.name)
+                    print(player_3.name, "you do the challenge, good luck")
+                elif p_4==1:
+                    list_challenge.append(player_4.name)
+                    print(player_4.name, "you do the challenge, good luck")
+                else:
+                    list_challenge.append("no challenge")
+                    print (list_challenge)
             elif p_1==5:
                 player_1.coins-=7
                 k_o=input("wich player lose 1 influence")
@@ -62,7 +93,7 @@ class influences (self,player_1,player_2,player_3,player_4):
                         #preguntar a los jugadores si quieren challenge
                     else:
                         player_1.coins+=2
-             if p_1==2: 
+            if p_1==2: 
                 player_1.coins -=3
                 print("To defense this attack you will need the Countess")
                 print(attack)
@@ -71,7 +102,7 @@ class influences (self,player_1,player_2,player_3,player_4):
                     #hacer elegir que carta pierde
                 else:
                     #preguntar a los jugadores si quieren hacer un challenge 
-            if p_1==3:
+            elif p_1==3:
                 print ("To defense this attack you will need the ambassador or the captain")
                 print (attack)
             ask=int(input("Do you whant to defense? 1= yes 2= no"))
@@ -79,3 +110,9 @@ class influences (self,player_1,player_2,player_3,player_4):
                     #hacer un if el jugador atacado tiene menos de 2 monedas robar 1 else robar 2
                 else:
                     #preguntar a los jugadores si quieren hacer un challenge 
+            elif p_1==4:
+                #permitir ver dos cartas del mazo y elegir con cuales se queda
+            elif p_1==1:
+                player_1.coins +=3
+            else: 
+                continue
