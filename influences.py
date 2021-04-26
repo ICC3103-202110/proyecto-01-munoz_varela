a2965880b048a84e3e4123459ee8f3eff7efe263
sebastianvarelab <svarela@miuandes.cl>
2021-04-26 12:58:18 -0400

    avance_80_carac__influence_commit

--- influences.py
+++ influences.py
@@ -4805,26 +4805,6 @@ class Influences:
         ask_a=0
         ask_c=0
         ask_a_c=0
-        a.cards = ca1
-        a.vcards = vc1
-        a.name = nombre1
-        a.color = col1
-        A = col1
-        b.cards = ca2
-        b.vcards = vc2
-        b.name = nombre2
-        b.color = col2
-        B = col2
-        c.cards = ca3
-        c.vcards = vc3
-        c.name = nombre3
-        c.color = col3
-        C = col3
-        d.cards = ca4
-        d.vcards = vc4
-        d.name = nombre4
-        d.color = col4
-        D = col4
         if n_players==4 or n_players==3 or n_players==2:
             if n_players == 4:
                 A,B,C,D,E,a,b,c,d=cond(board,player_1,player_2,player_3,
@@ -4893,6 +4873,26 @@ class Influences:
                         ""+str(log[i+1]))
             else:
                 print("ok, lets play")
+            a.cards = ca1
+        a.vcards = vc1
+        a.name = nombre1
+        a.color = col1
+        A = col1
+        b.cards = ca2
+        b.vcards = vc2
+        b.name = nombre2
+        b.color = col2
+        B = col2
+        c.cards = ca3
+        c.vcards = vc3
+        c.name = nombre3
+        c.color = col3
+        C = col3
+        d.cards = ca4
+        d.vcards = vc4
+        d.name = nombre4
+        d.color = col4
+        D = col4
             print(A+a.name)
             log.append(a.name)
             ##This is use to know if the player win or lose the challenge
